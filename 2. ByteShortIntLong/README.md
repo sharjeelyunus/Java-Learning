d4d76815176ffe21aa18b13282768dade9f803fc
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-04-14 04:31:54 +0500

    ➕ Primitive Types

---
+++ 2. ByteShortIntLong/README.md
@@ -0,0 +1,10 @@
+## Primitive Types
+
+# Maximum and Minimum Values of Primitive Types
+
+- Int
+- Byte
+- Short
+- Long
+
+Also included the busted value which proves that if we excedes the limit it shows wrong answer.
---
+++ 2. ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximun Value = " + myMaxIntValue);
+        System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min Value = "+ (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximun Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximun Value = " + myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximun Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+    }
+}

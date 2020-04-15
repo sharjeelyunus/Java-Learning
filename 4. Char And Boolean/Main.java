ab0f743794edbaa1d2cb5d710fd50b37ebe56965
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-04-16 01:40:13 +0500

    🎉Initial Commit

---
+++ 4. Char And Boolean/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        char myCopyRight = '\u00A9';
+        System.out.println(myCopyRight);
+
+        boolean isCustomerovertwentyOne = true;
+        System.out.println(isCustomerovertwentyOne);
+    }
+}

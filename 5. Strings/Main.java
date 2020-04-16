b90682177aae9380b5ea2ebb30a60d5408b4ad9b
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-04-17 02:28:14 +0500

    🎉Strings

---
+++ 5. Strings/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+    String name = "My name is Sharjeel Yunus.\nI'm Computer Science Student at The Islamia University Of Bahwalpur.";
+    System.out.println(name);
+    }
+}

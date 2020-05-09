6f7e5d14ed99d1ecab37ab8a934c2b35ba88c3a8
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-05-10 00:11:08 +0500

    🎉Keywords And Expressions

---
+++ 7. KeywordsAndExpressions/src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int int2=5;
+	//false, true, null
+        //a mile is equal to 1.609344 kilometers
+        double killometers = (100 * 1.609344);
+        int highScore =50;
+        if (highScore == 50) {
+            System.out.println("This is an Expression");
+        }
+
+        int score = 100;
+        if (score > 90) {
+            System.out.println("You got the high score");
+            score = 0;
+        }
+    }
+}

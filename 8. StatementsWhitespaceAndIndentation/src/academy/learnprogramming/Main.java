dcf50931b692420be0401887b58580d552c0d704
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-05-11 07:21:31 +0500

    🎉StatementsWhitespaceAndIndentation

---
+++ 8. StatementsWhitespaceAndIndentation/src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a Test");
+        System.out.println("This is" +
+                " another" +
+                " Still more");
+        int anotherVariable =50;myVariable--;System.out.println("This is another");
+    }
+}

73f2f90a6f67cd326aebda86350f0690e43554a4
sharjeelyunus <sharjeel924@gmail.com>
2020-06-23 23:57:27 +0500

    👌 IMPROVE: Methods

--- 10. Methods/src/me/sharjeelyunus/Main.java
+++ 10. Methods/src/me/sharjeelyunus/Main.java
@@ -9,23 +9,60 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score is: " + highScore);
 
-        score = 1000;
+        score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score is: " + highScore);
 
+        int highScorePosition = calculateHighScorePosition(1500);
+        displyHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displyHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displyHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displyHighScorePosition("Gilbert", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displyHighScorePosition("Louise", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displyHighScorePosition("Carol", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displyHighScorePosition("Frank", highScorePosition);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score is: " + finalScore);
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displyHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into postion " + highScorePosition + " on high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }

138d9d58b480422ed168c391ad8ed5bc2550958d
sharjeelyunus <sharjeel924@gmail.com>
2020-06-27 01:28:13 +0500

    Method Overloading

---
+++ 11. MethodOverloading/src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Sharjeel", 500);
+        System.out.println("New Score is " + newScore);
+
+        calculateScore(75);
+        calculateScore();
+
+        double centimeters = calcFeetAndInchesToCentimeters(6, 0);
+        if (centimeters <0.0) {
+            System.out.println("Invalid parameters");
+        }
+
+        calcFeetAndInchesToCentimeters(100);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed Player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+}

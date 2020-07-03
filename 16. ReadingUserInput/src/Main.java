7cf5a459b9bf683ca747dc9f32eb0b1dbf774dba
sharjeelyunus <sharjeel924@gmail.com>
2020-07-04 01:58:51 +0500

    🐛 FIX: Reading User Input

--- 16. ReadingUserInput/src/Main.java
+++ 16. ReadingUserInput/src/Main.java
@@ -6,14 +6,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.println("Enter Your Name: ");
-        String name = scanner.nextLine();
-        int age = 2020 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter Your Name: ");
+            String name = scanner.nextLine();
+            int age = 2020 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid Year of Birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
 
         scanner.close();
     }

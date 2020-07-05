79ad5329083a9fd3a242973105be55db6140200f
sharjeelyunus <sharjeel924@gmail.com>
2020-07-05 20:41:05 +0500

    🎉Classes

---
+++ 17. Classes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porsche = new Car();
+	    Car holden = new Car();
+
+	    porsche.setModel("Carrera");
+
+        System.out.println("Model is " + porsche.getModel());
+    }
+}

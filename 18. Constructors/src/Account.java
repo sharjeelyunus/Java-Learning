ccdb23caed8ffa975e7fa1448a489952afc242ba
sharjeelyunus <sharjeel924@gmail.com>
2020-07-07 00:53:54 +0500

    👌 IMPROVE: Constructors

--- 18. Constructors/src/Account.java
+++ 18. Constructors/src/Account.java
@@ -19,6 +19,10 @@ public class Account {
         System.out.println("Account Constructor with parameters called");
     }
 
+    public Account(String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        this("9999", 100.55, customerName, customerEmailAddress, customerPhoneNumber);
+    }
+
     public void deposit(double depositAmount) {
         this.balance += depositAmount;
         System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
--- 18. Constructors/src/Main.java
+++ 18. Constructors/src/Main.java
@@ -4,13 +4,25 @@ public class Main {
 
         Account bobsAccount = new Account("12345", 0.00, "Bob Brown", "myemail@bob.com", "+92 308 1234567");
 
-        bobsAccount.withdrawal(100.0);
+       bobsAccount.withdrawal(100.0);
 
-        bobsAccount.deposit(50.0);
-        bobsAccount.withdrawal(100.0);
+       bobsAccount.deposit(50.0);
+       bobsAccount.withdrawal(100.0);
 
-        bobsAccount.deposit(51.0);
-        bobsAccount.withdrawal(100.0);
+       bobsAccount.deposit(51.0);
+       bobsAccount.withdrawal(100.0);
 
+       Account timsAccount = new Account("Tim", "tim@email.com", "12345");
+       System.out.println(timsAccount.getNumber() + " name " + timsAccount.getCustomerName());
+
+
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
     }
 }

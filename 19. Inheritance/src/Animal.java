9dbe07a2b1d4b176c168809f0a28218312dc9b57
sharjeelyunus <sharjeel924@gmail.com>
2020-07-09 00:49:06 +0500

    👌 IMPROVE: Inheritance

--- 19. Inheritance/src/Animal.java
+++ 19. Inheritance/src/Animal.java
@@ -17,8 +17,8 @@ public class Animal {
         System.out.println("Animal.eat() called");
     }
 
-    public void move() {
-
+    public void move(int speed) {
+        System.out.println("Animal is moving at " + speed);
     }
 
     public String getName() {
--- 19. Inheritance/src/Main.java
+++ 19. Inheritance/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
 
 	    Dog dog = new Dog("Yorkie", 8,20, 2,4,1,20,"long silky");
 	    dog.eat();
+	    dog.walk();
+	    dog.run();
     }
 }

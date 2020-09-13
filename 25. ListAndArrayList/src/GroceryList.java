4ce87bacd341cffac08d95a2fec7483254be92c8
sharjeelyunus <sharjeel924@gmail.com>
2020-09-13 23:15:57 +0500

    👌 IMPROVE: ArrayList

--- 25. ListAndArrayList/src/GroceryList.java
+++ 25. ListAndArrayList/src/GroceryList.java
@@ -23,4 +23,13 @@ public class GroceryList {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
     }
+
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+        return null;
+    }
 }
